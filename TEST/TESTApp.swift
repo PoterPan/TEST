863a29a32256614f8f5a0611a411738032da7349
PoterPan <poterpan5466@gmail.com>
2022-05-31 18:49:22 +0800

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by Poter Pan on 2022/5/31.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
