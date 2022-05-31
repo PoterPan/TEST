6d0eaf5d62ab767b8d394c57d3a850cd430a14c9
PoterPan <poterpan5466@gmail.com>
2022-05-31 19:07:24 +0800

    first commit

--- TEST/ContentView.swift
+++ TEST/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Color.blue
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
